7a19d3a5a6e5f9383046a1e964d838c46d5d9034
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-12-08 01:50:22 -0600

    Cambio Foto Perfil
    Se agrego un cambio para la foto de perfil, ademas de que se borro un aspecto redundante en el proyecto

--- app/src/main/java/com/example/proyectofinalmovil3/PerfilFragment.kt
+++ app/src/main/java/com/example/proyectofinalmovil3/PerfilFragment.kt
@@ -1,32 +1,40 @@
 package com.example.proyectofinalmovil3
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.google.firebase.Firebase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.database
+import java.io.File
 
 class PerfilFragment : Fragment() {
 
     // Variables de entorno gráfico
+    private lateinit var imageView: ImageView
     private lateinit var nombreUsuario: TextView
     private lateinit var nombreUsuarioEditText: TextView
     private lateinit var correoUsuario: TextView
 
     private lateinit var editarPerfilButton: Button
     private lateinit var actualizarButton: Button
+    private lateinit var cambiarFotoButton: Button
 
-    private lateinit var eventosTextView: TextView
-    private lateinit var pasosTextView: TextView
-    private lateinit var logrosTextView: TextView
 
     private lateinit var cerrarSesionButton: Button
     private lateinit var adminButton: Button
@@ -36,6 +44,27 @@ class PerfilFragment : Fragment() {
 
     // SharedPreferences
     private lateinit var sharedPreferences: SharedPreferences
+
+    private lateinit var fotoUri: Uri
+
+    private val launcherCamara = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { success ->
+        if (success) {
+            imageView.setImageURI(fotoUri)
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                abrirCamara()
+            } else {
+                Toast.makeText(requireContext(), "Se necesita permiso de cámara", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,16 +80,14 @@ class PerfilFragment : Fragment() {
         database = Firebase.database.reference
 
         // Inicialización de vistas
+        imageView = view.findViewById(R.id.imageView)
         nombreUsuario = view.findViewById(R.id.nombreUsuario)
         nombreUsuarioEditText = view.findViewById(R.id.nombreUsuarioEditText)
         correoUsuario = view.findViewById(R.id.correoUsuario)
 
         editarPerfilButton = view.findViewById(R.id.editarPerfilButton)
         actualizarButton = view.findViewById(R.id.actualizarButton)
-
-        eventosTextView = view.findViewById(R.id.eventosTextView)
-        pasosTextView = view.findViewById(R.id.pasosTextView)
-        logrosTextView = view.findViewById(R.id.logrosTextView)
+        cambiarFotoButton = view.findViewById(R.id.cambiarFotoButton)
 
         cerrarSesionButton = view.findViewById(R.id.cerrarSesionButton)
         adminButton = view.findViewById(R.id.adminButton)
@@ -79,9 +106,31 @@ class PerfilFragment : Fragment() {
             val intent = Intent(requireContext(), ActivityAdmin::class.java)
             startActivity(intent)
         }
+        cambiarFotoButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+
+                abrirCamara() // ✔ permiso dado
+
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA) // ❗ pedir permiso
+            }
+
+        }
 
         return view
     }
+    private fun abrirCamara() {
+        val archivo = File(requireContext().externalCacheDir, "foto_temp.jpg")
+        fotoUri = FileProvider.getUriForFile(
+            requireContext(),
+            "${requireContext().packageName}.provider",
+            archivo
+        )
+
+        launcherCamara.launch(fotoUri)
+    }
+
 
     private fun cerrarSesion() {
         sharedPreferences.edit().clear().apply()
