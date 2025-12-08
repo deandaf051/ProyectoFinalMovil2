7bdc75e19f0d27a9a918ee00a8c19823cdddc5e6
Juan Pablo <juan_pablo010@live.com.mx>
2025-12-08 02:16:24 -0600

    Desarrollo de MapaFragment.kt

--- app/src/main/java/com/example/proyectofinalmovil3/Evento/Evento.kt
+++ app/src/main/java/com/example/proyectofinalmovil3/Evento/Evento.kt
@@ -3,12 +3,13 @@ package com.example.proyectofinalmovil3.Evento
 import java.io.Serializable
 
 data class Evento(
-    val mes: String,
-    val dia: String,
-    val titulo: String,
-    val descripcion: String,
-    val categoria: String,
-    val inscritos: Int,
-    val hora: String,
-    val lugar: String
+    // Añadimos valores por defecto a todos los campos
+    val mes: String = "",
+    val dia: String = "",
+    val titulo: String = "",
+    val descripcion: String = "",
+    val categoria: String = "",
+    val inscritos: Int = 0,
+    val hora: String = "",
+    val lugar: String = ""
 ) : Serializable
