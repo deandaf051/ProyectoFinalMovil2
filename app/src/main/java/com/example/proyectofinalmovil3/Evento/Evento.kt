4a15e459da42a7c2d7579aa0b86e208d66fac1de
deandaf051 <77902558+deandaf051@users.noreply.github.com>
2025-12-08 01:00:56 -0600

    prueba

--- app/src/main/java/com/example/proyectofinalmovil3/Evento/Evento.kt
+++ app/src/main/java/com/example/proyectofinalmovil3/Evento/Evento.kt
@@ -1,12 +1,12 @@
 package com.example.proyectofinalmovil3.Evento
 
 data class Evento(
-    val mes: String = "",
-    val dia: String = "",
-    val titulo: String = "",
-    val descripcion: String = "",
-    val categoria: String = "",
-    val inscritos: Int = 0,
-    val hora: String = "",
-    val lugar: String = ""
+    val mes: String,
+    val dia: String,
+    val titulo: String,
+    val descripcion: String,
+    val categoria: String,
+    val inscritos: Int,
+    val hora: String,
+    val lugar: String
 )
